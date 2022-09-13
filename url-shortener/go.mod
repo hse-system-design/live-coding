221ea6fe4aeedb2324b10671293f2a5705fd3220
Lev Khotov <lfyuomr-gylo@yandex.ru>
2022-09-14 00:37:02 +0200

    [week03] Integrate OpenAPI validation into tests.
    (cherry picked from commit 8df2ee3f0404d40c63ae9017f8abbabc1151982d)

--- url-shortener/go.mod
+++ url-shortener/go.mod
@@ -3,12 +3,18 @@ module url-shortener
 go 1.19
 
 require (
+	github.com/getkin/kin-openapi v0.75.0
 	github.com/gorilla/mux v1.8.0
 	github.com/stretchr/testify v1.8.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/swag v0.19.5 // indirect
+	github.com/mailru/easyjson v0.0.0-20190626092158-b2ccc519800e // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- url-shortener/main_test.go
+++ url-shortener/main_test.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	_ "embed"
 	"encoding/json"
 	"fmt"
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/getkin/kin-openapi/openapi3filter"
+	openapi3_routers "github.com/getkin/kin-openapi/routers"
+	openapi3_legacy "github.com/getkin/kin-openapi/routers/legacy"
 	"github.com/stretchr/testify/suite"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
 )
+
+//go:embed api.yaml
+var apiSpec []byte
+
+var ctx = context.Background()
 
 func TestAPI(t *testing.T) {
 	suite.Run(t, &APISuite{})
@@ -21,7 +33,8 @@ func TestAPI(t *testing.T) {
 type APISuite struct {
 	suite.Suite
 
-	client http.Client
+	client        http.Client
+	apiSpecRouter openapi3_routers.Router
 }
 
 func (s *APISuite) SetupSuite() {
@@ -30,6 +43,14 @@ func (s *APISuite) SetupSuite() {
 		log.Printf("Start serving on %s", srv.Addr)
 		log.Fatal(srv.ListenAndServe())
 	}()
+
+	spec, err := openapi3.NewLoader().LoadFromData(apiSpec)
+	s.Require().NoError(err)
+	s.Require().NoError(spec.Validate(ctx))
+	router, err := openapi3_legacy.NewRouter(spec)
+	s.Require().NoError(err)
+	s.apiSpecRouter = router
+	s.client.Transport = s.specValidating(http.DefaultTransport)
 }
 
 func (s *APISuite) TestNotFound() {
@@ -50,7 +71,7 @@ func (s *APISuite) TestCreateAndGet() {
 	}))
 
 	var key string
-	s.Run("CreateShortcut", func() {
+	s.Run("CheckRedirectResponse", func() {
 		// when:
 		reqBody := io.NopCloser(strings.NewReader(fmt.Sprintf( /* language=json */ `{"url": "%s"}`, testServer.URL)))
 		resp, err := s.client.Post("http://localhost:8080/api/urls", "application/json", reqBody)
@@ -83,8 +104,11 @@ func (s *APISuite) TestCreateAndGet() {
 	})
 
 	s.Run("CheckFollowingRedirect", func() {
+		// setup:
+		var client http.Client // don't validate against spec and follow redirects
+
 		// when:
-		resp, err := s.client.Get(fmt.Sprintf("http://localhost:8080/%s", key))
+		resp, err := client.Get(fmt.Sprintf("http://localhost:8080/%s", key))
 
 		// then:
 		s.Require().NoError(err)
@@ -93,4 +117,78 @@ func (s *APISuite) TestCreateAndGet() {
 		s.Require().NoError(err)
 		s.Require().Equal(targetContent, body)
 	})
+}
+
+func (s *APISuite) specValidating(transport http.RoundTripper) http.RoundTripper {
+	return RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
+		log.Println("Send HTTP request:")
+		reqBody := s.printReq(req)
+
+		// validate request
+		route, params, err := s.apiSpecRouter.FindRoute(req)
+		s.Require().NoError(err)
+		reqDescriptor := &openapi3filter.RequestValidationInput{
+			Request:     req,
+			PathParams:  params,
+			QueryParams: req.URL.Query(),
+			Route:       route,
+		}
+		s.Require().NoError(openapi3filter.ValidateRequest(ctx, reqDescriptor))
+
+		// do request
+		req.Body = io.NopCloser(bytes.NewReader(reqBody))
+		resp, err := transport.RoundTrip(req)
+		if err != nil {
+			return nil, err
+		}
+		log.Println("Got HTTP response:")
+		respBody := s.printResp(resp)
+
+		// Validate response against OpenAPI spec
+		s.Require().NoError(openapi3filter.ValidateResponse(ctx, &openapi3filter.ResponseValidationInput{
+			RequestValidationInput: reqDescriptor,
+			Status:                 resp.StatusCode,
+			Header:                 resp.Header,
+			Body:                   io.NopCloser(bytes.NewReader(respBody)),
+		}))
+
+		return resp, nil
+	})
+}
+
+func (s *APISuite) printReq(req *http.Request) []byte {
+	body := s.readAll(req.Body)
+
+	req.Body = io.NopCloser(bytes.NewReader(body))
+	s.Require().NoError(req.Write(os.Stdout))
+	fmt.Println()
+
+	req.Body = io.NopCloser(bytes.NewReader(body))
+	return body
+}
+
+func (s *APISuite) printResp(resp *http.Response) []byte {
+	body := s.readAll(resp.Body)
+
+	resp.Body = io.NopCloser(bytes.NewReader(body))
+	s.Require().NoError(resp.Write(os.Stdout))
+	fmt.Println()
+
+	resp.Body = io.NopCloser(bytes.NewReader(body))
+	return body
+}
+
+func (s *APISuite) readAll(in io.Reader) []byte {
+	if in == nil {
+		return nil
+	}
+	data, err := ioutil.ReadAll(in)
+	s.Require().NoError(err)
+	return data
+}
+
+type RoundTripperFunc func(*http.Request) (*http.Response, error)
+
+func (fn RoundTripperFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return fn(req)
 }
