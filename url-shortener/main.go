a6b8dc044ac3e52b59c3dc09a7fec0d589efd7c9
Lev Khotov <lfyuomr-gylo@yandex.ru>
2022-09-27 23:41:58 +0200

    [week04] support alternative run mode.
    (cherry picked from commit e24d603f00f71f5d735178040e6a93bec8c83b56)

--- url-shortener/main.go
+++ url-shortener/main.go
@@ -1,15 +1,36 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"sync"
 	"url-shortener/grpcapi"
 	"url-shortener/httpapi"
+	"url-shortener/urlshortener"
 	"url-shortener/urlshortener/inmemoryimpl"
+	"url-shortener/urlshortener/mongoimpl"
 )
 
+const (
+	modeInMemory = "in-memory"
+	modeMongo    = "mongo"
+)
+
+var flagMode = flag.String("mode", modeInMemory, fmt.Sprintf("Storage mode. Possible values: %q, %q", modeInMemory, modeMongo))
+var flagMongoAddr = flag.String("mongo-addr", "mongodb://localhost:27017", "Address of MongoDB to connect to")
+
 func main() {
-	manager := inmemoryimpl.NewManager()
+	flag.Parse()
+	var manager urlshortener.Manager
+	switch *flagMode {
+	case modeInMemory:
+		manager = inmemoryimpl.NewManager()
+	case modeMongo:
+		manager = mongoimpl.NewManager(*flagMongoAddr)
+	default:
+		log.Fatalf("Unexpected mode flag: %q", *flagMode)
+	}
 
 	var wg sync.WaitGroup
 	wg.Add(2)
